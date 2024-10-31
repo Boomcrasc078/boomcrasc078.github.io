0c7556d7fb7bc75e4806585ff708edf729a7be8b
Boomcrasc078 <mose.g07@gmail.com>
2024-10-31 15:22:29 +0100

    Started on All Races

---
+++ RaceTimer/Classes/Racer.cs
@@ -0,0 +1,11 @@
+﻿namespace RaceTimer.Classes
+{
+	public class Racer
+	{
+		public string name { get; set; }
+		public string surname { get; set; }
+		public string bib { get; set; }
+		public DateTime startDateTime { get; set; }
+		public List<DateTime> lapDateTime { get; set; }
+	}
+}
