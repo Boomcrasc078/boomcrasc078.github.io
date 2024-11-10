9f3e1d7b82475de166207356882809e6621b30e9
Boomcrasc078 <mose.g07@gmail.com>
2024-11-10 15:37:42 +0100

    Publish

--- RaceTimer/Classes/Racer.cs
+++ RaceTimer/Classes/Racer.cs
@@ -9,7 +9,7 @@
 		public string Surname { get; set; } = string.Empty;
 		public string Bib { get; set; } = string.Empty;
 		public string Id { get; set; } = string.Empty;
-		public DateTime? StartDateTime { get; set; }
+		public DateTime? StartDateTime { get; set; } = null;
 		public List<DateTime> LapDateTime { get; set; } = new List<DateTime>();
 		public string currentAnimation { get; set; } = string.Empty;
 		public List<CustomField> CustomFields { get; set; } = new List<CustomField>();
