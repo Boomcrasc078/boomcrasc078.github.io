c321c25787eff49ac4a29eed648a73f1d38ecda7
Boomcrasc078 <mose.g07@gmail.com>
2024-11-15 16:34:10 +0100

    Edit TimeRace.razor Trying to fix toasts

---
+++ RaceTimer/Classes/Toast.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Toast
+{
+	public string id { get; set; }
+	public string title { get; set; }
+	public string text { get; set; }
+
+	public Toast(string id, string title, string text)
+	{
+		this.id = id;
+		this.title = title;
+		this.text = text;
+	}
+}
