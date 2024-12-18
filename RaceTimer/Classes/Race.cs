72cff9c2a37be969921a737be589e4f3431ac7f9
Boomcrasc078 <mose.g07@gmail.com>
2024-12-18 17:50:55 +0100

    Fixing design All Races

--- RaceTimer/Classes/Race.cs
+++ RaceTimer/Classes/Race.cs
@@ -10,4 +10,27 @@ public class Race
     public DateTime creationDateTime { get; set; } = new DateTime();
     public DateTime lastEditDateTime { get; set; } = new DateTime();
     public string currentAnimation { get; set; } = "";
+
+	public static string TimeSpanToString(TimeSpan timeSpan)
+	{
+		if (timeSpan.TotalSeconds < 60)
+			return timeSpan.Seconds + " seconds ago";
+
+		if (timeSpan.TotalMinutes < 60)
+			return timeSpan.Minutes + " minutes ago";
+
+		if (timeSpan.TotalHours < 24)
+			return timeSpan.Hours + " hours ago";
+
+		if (timeSpan.TotalDays < 7)
+			return timeSpan.Days + " days ago";
+
+		if (timeSpan.TotalDays < 30)
+			return Math.Floor(timeSpan.TotalDays / 7) + " weeks ago";
+
+		if (timeSpan.TotalDays < 365)
+			return Math.Floor(timeSpan.TotalDays / 30) + " months ago";
+
+		return Math.Floor(timeSpan.TotalDays / 365) + " years ago";
+	}
 }
